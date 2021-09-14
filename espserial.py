bf2dc974130ebbcccaeeedd18f2e5d8c09edbff2
Ivan <capobianco.ivan@gmail.com>
2021-09-14 17:38:33 +0200

    refactor: move wmi data filterring to own file

--- espserial.py
+++ espserial.py
@@ -1,7 +1,7 @@
 import serial
 from multiprocessing import Process
 
-from utils import process_wmi_data_for_esp32
+from wmifilter import process_wmi_data_for_esp32
 
 class EspSerialProcess(Process):
     def __init__(self, wmi_data_queue, port):
@@ -17,16 +17,16 @@ class EspSerialProcess(Process):
             while self.serial_port.isOpen():
                 data = self.wmi_data_queue.get()
                 processed_data = process_wmi_data_for_esp32(data)
-                print(processed_data)
+                # print(processed_data)
                 self.serial_port.write(processed_data.encode("ascii"))
                 self.serial_port.flush()
                 
-                try:
-                    incoming = self.serial_port.read_all().decode("utf-8")
-                    # incoming = self.serial_port.readline().decode("utf-8")
-                    print(incoming)
-                except Exception as e:
-                    print(e)
+                # try:
+                #     incoming = self.serial_port.read_all().decode("utf-8")
+                #     # incoming = self.serial_port.readline().decode("utf-8")
+                #     print(incoming)
+                # except Exception as e:
+                #     print(e)
         except Exception as e:
             print(e)
             return
--- utils.py
+++ utils.py
@@ -1,43 +1,5 @@
-import enum
 from serial.tools import list_ports
-import json
-import re
-from enum import Enum
 
 def get_serial_ports():
     """Return a list of all serial ports found"""
     return sorted(list_ports.comports())
-
-class DrawingType(Enum):
-    ARC = 0
-    LINE = 1
-
-def process_wmi_data_for_esp32(wmi_data_json):
-    data = json.loads(wmi_data_json)
-    new_data = []
-
-    idx = 0
-    for hardware in data:
-        sensor_array = hardware.pop("s")
-
-        if re.search("cpu|gpu", hardware["i"]):
-            hardware["t"] = DrawingType.ARC.value
-        if re.search("ram", hardware["i"]):
-            hardware["n"] = hardware["t"]
-            hardware["t"] = DrawingType.LINE.value
-        
-        hardware.pop("i")
-        hardware["s"] = []
-        new_data.append(hardware)
-
-        for sensor in sensor_array:
-            if re.search("^CPU Package$|^CPU Total$|^GPU Core$|^Memory$", sensor["n"]):
-                sensor["v"] = round(sensor["v"], 1)
-                new_data[idx]["s"].append(sensor)
-
-        # sort sensors by type
-        new_data[idx]["s"] = sorted(new_data[idx]["s"], key=lambda k: k["t"])
-
-        idx += 1
-
-    return json.dumps(sorted(new_data, key=lambda k: (k['t'], k['n'])))
